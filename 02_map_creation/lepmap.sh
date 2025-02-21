7a46613bbbad8da0e365f236548e207566919a96
Amelia Lawrence <55457044+ahlawrence@users.noreply.github.com>
2025-02-21 16:32:07 -0500

    Update lepmap.sh

--- 02_map_creation/lepmap.sh
+++ 02_map_creation/lepmap.sh
@@ -9,3 +9,10 @@ theta=''
 java -cp ~/.conda/envs/java8/bin/binary+code/bin/ SeparateChromosomes2 numThreads=8 grandparentPhase=1  data=lm.call lodLimit=$lod theta=$theta > map.$lod.$theta.pc.txt
 java -cp   ~/.conda/envs/java8/bin/binary+code/bin/ OrderMarkers2 numThreads=8 grandparentPhase=1 data=lm.call map=map.$lod.$theta.pc.txt > order.$lod.$theta.pc.txt
 
+
+#### Please Download map2genotypes.awk from the Lep-MAP3 Wiki ########
+awk -vfullData=1 -f map2genotypes.awk order1.txt >qtldata1.12
+cat data.call|cut -f 1,2|awk '(NR>=7)' >snps.txt
+#note that first line of snps.txt contains "CHR POS"
+awk -vFS="\t" -vOFS="\t" '(NR==FNR){s[NR-1]=$0}(NR!=FNR){if ($1 in s) $1=s[$1];print}' snps.txt qtldata1.12 > qtl_data.txt
+#because of first line of snps.txt, we use NR-1 instead of NR
