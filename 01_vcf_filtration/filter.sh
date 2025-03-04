787dede59296b6478b8a94fd0bf055375d91b5d3
Amelia Lawrence <55457044+ahlawrence@users.noreply.github.com>
2025-03-04 10:48:00 -0500

    Update filter.sh

--- 01_vcf_filtration/filter.sh
+++ 01_vcf_filtration/filter.sh
@@ -1,8 +1,10 @@
+#add file name here
+PRE=""
 
 
 
-bcftools view -h ${PRE}$f.filtered.vcf > ${PRE}$c.norepeats.vcf 
-bedtools intersect -v -a ${PRE}$f.filtered.vcf -b /work/eo22/01_mapping/repeats_tol.bed >>  ${PRE}$c.norepeats.vcf 
-vcftools --vcf ${PRE}$c.norepeats.vcf --thin 150 --recode --recode-INFO-all --stdout > ${PRE}$c.thinned.vcf 
+bcftools view -h ${PRE}.filtered.vcf > ${PRE}.norepeats.vcf 
+bedtools intersect -v -a ${PRE}.filtered.vcf -b repeats_tol.bed >>  ${PRE}.norepeats.vcf 
+vcftools --vcf ${PRE}.norepeats.vcf --thin 150 --recode --recode-INFO-all --stdout > ${PRE}.thinned.vcf 
 
-java -cp ~/.conda/envs/java8/bin/binary+code/bin/ ParentCall2 data= /work/ahl28/QTL_Mapping/lepmap/IM_CS.ped.txt vcfFile = ${PRE}$c.thinned.vcf removeNonInformative=1 > $c.call
+java -cp ~/.conda/envs/java8/bin/binary+code/bin/ ParentCall2 data=ped.txt vcfFile = ${PRE}.thinned.vcf removeNonInformative=1 > ${PRE}.call
